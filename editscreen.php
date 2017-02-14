ea0c402d93b396d4e1c035154b4ebe73bfd80f58
Mayank Shekhar <mayankshekhar03@gmail.com>
2017-02-14 13:23:26 +0530

    improvements in ui

--- editscreen.php
+++ editscreen.php
@@ -2,9 +2,26 @@
 	include("dbh.php");
 	session_start();
 ?>
+<!DOCTYPE html>
 <html>
-<head>
+<head lang="en">
 	<title>Add a Notice</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>WordOut</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<style>
+	.bg-1 { 
+    background-color: #1abc9c; /* Green */
+    color: #ffffff;
+	}
+	</style>
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php
@@ -17,7 +34,8 @@ $sql = "SELECT bid, msg, date FROM msgs WHERE bid ='$bid'";
 	   if(! $retval ) {
 	      die('Could not get data.');
 	   }
-	   
+	   echo '<div class="container-fluid text-center">
+	   <div class="row  bg-1"><div class="col-sm-12"><h2>'.$bid.'</h2></div></div></div>';
 	   while($row = $retval->fetch_assoc()) {
 		      echo "bid :{$row['bid']}  <br> ".
 		         "msg : {$row['msg']} <br> ".
